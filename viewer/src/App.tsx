a05f3644806ea02bb3f449551d4576ed5f64ba66
kigabor91 <kigabor91@gmail.com>
2026-07-15 15:28:26 +0200

    Create SymetrIQ application shell

---
+++ viewer/src/App.tsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#1e1e1e",
+        color: "white",
+        fontFamily: "Arial",
+        fontSize: "32px",
+        fontWeight: "bold",
+      }}
+    >
+      SymetrIQ Viewer
+    </div>
+  );
+}
+
+export default App;
