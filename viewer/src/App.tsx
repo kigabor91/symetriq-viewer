b0b626958ecda4e00d36a3ed76617f3580776c6d
kigabor91 <kigabor91@gmail.com>
2026-07-15 16:34:58 +0200

    Create Viewer component

--- viewer/src/App.tsx
+++ viewer/src/App.tsx
@@ -1,22 +1,7 @@
+import Viewer from "./components/Viewer";
+
 function App() {
-  return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        background: "#1e1e1e",
-        color: "white",
-        fontFamily: "Arial",
-        fontSize: "32px",
-        fontWeight: "bold",
-      }}
-    >
-      SymetrIQ Viewer
-    </div>
-  );
+    return <Viewer />;
 }
 
 export default App;
---
+++ viewer/src/components/Viewer.tsx
@@ -0,0 +1,14 @@
+function Viewer() {
+    return (
+        <div
+            id="xeokit-canvas"
+            style={{
+                width: "100%",
+                height: "100%",
+                backgroundColor: "#202124",
+            }}
+        />
+    );
+}
+
+export default Viewer;
