e5f9d0556e4aaa37ff4c983c28419f6620da0f69
kigabor91 <kigabor91@gmail.com>
2026-07-15 16:37:10 +0200

    Prepare canvas for xeokit

--- viewer/src/components/Viewer.tsx
+++ viewer/src/components/Viewer.tsx
@@ -1,10 +1,19 @@
+import { useEffect, useRef } from "react";
+
 function Viewer() {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        console.log("Viewer initialized");
+    }, []);
+
     return (
-        <div
-            id="xeokit-canvas"
+        <canvas
+            ref={canvasRef}
             style={{
                 width: "100%",
                 height: "100%",
+                display: "block",
                 backgroundColor: "#202124",
             }}
         />
